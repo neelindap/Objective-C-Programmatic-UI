3bce875abc5763a2ca35eb93b163e5166e44ed53
rab <rab@fatima-zohra-mataouis-macbook-pro.local>
2016-10-23 07:42:57 -0400

    Initial Commit

---
+++ EmptyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmptyApp
+//
+//  Created by rab on 10/23/16.
+//  Copyright © 2016 rab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EmptyApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EmptyApp
+//
+//  Created by rab on 10/23/16.
+//  Copyright © 2016 rab. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
